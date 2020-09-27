e55eb47d21e1648c365501257d3510bfc91b3191
boxmein <wsdf294@gmail.com>
2020-09-27 22:50:00 +0300

    run.sh works + homebrew not updating yet

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -eu
-ansible-galaxy collection install --ignore-errors community.general markosamuli.asdf
+ansible-galaxy collection install --ignore-errors community.general
+ansible-galaxy install --ignore-errors markosamuli.asdf
 ansible-pull -U git@github.com:boxmein/computer-setup -i hosts local.yml
