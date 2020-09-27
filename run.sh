1cf1fa2ec418da28693e878cba20dc041ae8f005
boxmein <wsdf294@gmail.com>
2020-09-27 22:47:20 +0300

    Add homebrew, neovim, asdf setups. Hope it works

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -eu
-ansible-galaxy collection install --ignore-errors community.general
+ansible-galaxy collection install --ignore-errors community.general markosamuli.asdf
 ansible-pull -U git@github.com:boxmein/computer-setup -i hosts local.yml
